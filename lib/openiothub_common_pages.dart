6e4eb7eb63fe0d5057af8298ca8777b2d82f4065
Yu Fang <yu@iotserv.com>
2021-04-23 18:16:35 +0800

    publish

--- lib/openiothub_common_pages.dart
+++ lib/openiothub_common_pages.dart
@@ -1,7 +1,15 @@
 library openiothub_common_pages;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'commPages/appInfo.dart';
+export 'commPages/findmDNSClientList.dart';
+export 'commPages/settings.dart';
+
+export 'user/LoginPage.dart';
+export 'user/RegisterPage.dart';
+export 'user/userInfoPage.dart';
+
+export 'wifiConfig/airkiss.dart';
+export 'wifiConfig/easylink.dart';
+export 'wifiConfig/oneshot.dart';
+export 'wifiConfig/smartConfigTool.dart';
+export 'wifiConfig/smartlink.dart';
