21df67ee6fff7c43bbaf583be1a4daaf95ab46fa
Yu Fang <yu@iotserv.com>
2021-06-27 20:40:33 +0800

    添加微信登录绑定解绑相关

--- lib/user/userInfoPage.dart
+++ lib/user/userInfoPage.dart
@@ -1,12 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:iot_manager_grpc_api/pb/common.pb.dart';
-import 'package:openiothub_api/api/IoTManager/UserManager.dart';
 import 'package:openiothub_api/openiothub_api.dart';
 import 'package:openiothub_common_pages/user/LoginPage.dart';
 import 'package:openiothub_constants/openiothub_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:wechat_kit/wechat_kit.dart';
 
 class UserInfoPage extends StatefulWidget {
   @override
@@ -14,12 +16,35 @@ class UserInfoPage extends StatefulWidget {
 }
 
 class _UserInfoPageState extends State<UserInfoPage> {
+  StreamSubscription<WechatAuthResp> _auth;
+
   String username = "";
   String usermobile = "";
   String useremail = "";
 
+  Future<void> _listenAuth(WechatAuthResp resp) async {
+    if (resp.errorCode == 0 ) {
+      OperationResponse operationResponse =
+      await UserManager.BindWithWechatCode(resp.code);
+      if (operationResponse.code == 0) {
+        Fluttertoast.showToast(
+            msg: "绑定微信成功！");
+      }else{
+        Fluttertoast.showToast(
+            msg: "绑定微信失败:${operationResponse.msg}");
+      }
+    } else {
+      Fluttertoast.showToast(
+          msg: "获取微信登录信息失败:${resp.errorMsg}");
+    }
+  }
+
   @override
   void initState() {
+    if (_auth == null) {
+      _auth =
+          Wechat.instance.authResp().listen(_listenAuth);
+    }
     _getUserInfo();
     super.initState();
   }
@@ -60,22 +85,28 @@ class _UserInfoPageState extends State<UserInfoPage> {
                 _modifyInfo("密码");
               }),
           ListTile(
-            //绑定微信
+              //绑定微信
               title: Text('绑定微信'),
               trailing: Icon(Icons.arrow_right),
               onTap: () async {
-              //  TODO
-                Fluttertoast.showToast(
-                    msg: "绑定微信");
+                Wechat.instance.auth(
+                  scope: <String>[WechatScope.SNSAPI_USERINFO],
+                  state: 'auth',
+                );
               }),
           ListTile(
-            //解绑微信
+              //解绑微信
               title: Text('解除微信绑定'),
               trailing: Icon(Icons.arrow_right),
               onTap: () async {
-              //  TODO
-                Fluttertoast.showToast(
-                    msg: "解绑微信");
+                UserManager.UnbindWechat()
+                    .then((OperationResponse operationResponse) {
+                  if (operationResponse.code == 0) {
+                    Fluttertoast.showToast(msg: "解绑微信成功！");
+                  }else{
+                    Fluttertoast.showToast(msg: "解绑微信失败！原因：${operationResponse.msg}");
+                  }
+                });
               }),
           TextButton(
               onPressed: () {
